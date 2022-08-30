93b8598e0400a1da7a30155595353f55160c4731
Segerlu <steven.segerlund@yahoo.com>
2022-08-30 19:27:35 -0400

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// Your JS goes here
+// % for below, float property will be left, tile each as a div.
+let widthAndBottom = 11.1;
+
+const newDiv = document.createElement("div");
+
+for (let x = 0; x < 65; x++) {
+    body.appendChild(newDiv)
+}
+
+function randomColor() {
+
+    return [((Math.random*255)+1),((Math.random*255)+1),((Math.random*255)+1) ]
+}
+
