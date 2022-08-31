c3d45218f25c1fc895b736c3a12ab70e67943911
Segerlu <steven.segerlund@yahoo.com>
2022-08-30 20:26:45 -0400

    finished

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 // % for below, float property will be left, tile each as a div.
 let widthAndBottom = 11.1;
 
-const newDiv = document.createElement("div");
-
-for (let x = 0; x < 65; x++) {
-    body.appendChild(newDiv)
+let attributes = {
+    'width': '11.1%',
+    'padding-bottom': '11.1%',
+    'float': "left",
+    'background-color': randomColor()
 }
 
-function randomColor() {
+for (let x = 0; x < 63; x++) {
+    attributes['background-color'] = randomColor();
+    document.body.appendChild(createDiv());
+}
 
-    return [((Math.random*255)+1),((Math.random*255)+1),((Math.random*255)+1) ]
+function createDiv() {
+    let div = document.createElement("div");
+    Object.assign(div.style, attributes);
+    return div;
+}
+
+function randomColor(element) {
+    return `rgb(${((Math.random() * 255) + 1)},${((Math.random() * 255)) + 1},${((Math.random() * 255) + 1)})`
 }
 
